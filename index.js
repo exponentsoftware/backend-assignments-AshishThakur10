75080fa28a4c88e8112bff950146c0c4c306680b
AshishThakur10 <sendashishthakur@gmail.com>
2021-08-14 08:21:28 +0530

    mongoose
    used .commect method in mongoose

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const PORT = 4000
+
+mongoose.connect('mongodb+srv://Ash1:Ash1@cluster0.pbef3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+      dbName: 'todo',
+      useNewUrlParser : true,
+      useUnifiedTopology: true
+    
+    })
+    .then(()=> {
+    console.log('Mongodb connected……');
+    });
 
 app.get('/', function(req,res){
-res.send('hi welcome to this server')
+res.send('hi welcome to the server')
 })
 
-app.listen(4000);     //Listen on a port 
+app.listen(PORT, function(){console.log('server started at 4000')});     //Listen on a port 
